03c40dfe1c3f0365cf67f3aa4ee3fe69bf8185ad
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2025-07-29 20:57:57 +0530

    java basic

---
+++ java_basic.java
@@ -0,0 +1,15 @@
+public class java_basic{
+public static void main(String args[]){
+        System.out.println("hello");//ln use for change line
+
+        //variable in java
+        int a= 10;
+        int b=5;
+        System.out.println(2*(a+b));
+
+        // input in java
+        Scanner sc = new Scanner(System.in);
+        String input=  sc.nextLine();
+        System.out.println(input);
+    }
+}
