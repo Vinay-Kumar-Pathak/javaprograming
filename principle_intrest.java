dd0d2c5fb024067e2fd4a8ebc80cdd32d1b19197
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2025-07-30 13:07:03 +0530

    to find principal intrest

---
+++ principle_intrest.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class principle_intrest {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter Principal (P): ");
+    float principal = sc.nextFloat();
+    System.out.print("Enter Rate of Interest (R): ");
+    float rate = sc.nextFloat();
+    System.out.print("Enter Time in years (T): ");
+    float time = sc.nextFloat();
+    float si = (principal * rate * time) / 100;
+    System.out.println("Simple Interest = " + si);
+    sc.close();
+    }
+}
