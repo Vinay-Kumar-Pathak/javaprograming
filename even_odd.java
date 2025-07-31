2fb13126c8b079eed01d9001f638f9cd5b3ee127
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2025-07-31 15:41:15 +0530

    evenodd

--- even_odd.java
+++ even_odd.java
@@ -9,10 +9,10 @@ public class even_odd {
         int num = sc.nextInt();  // Number input
 
         // Check even or odd
-        if (num % 2 == 0) {
-            System.out.println(num + " is Even");
+        if (num % 2 != 0) {
+            System.out.println(num + " is odd");
         } else {
-            System.out.println(num + " is Odd");
+            System.out.println(num + " is even");
         }
     }
 }
