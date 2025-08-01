bdfe9fa42a3854bd72de570e98d092998c9c58be
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2025-08-01 09:14:22 +0530

    multiplly two float digit

---
+++ finding_greater_number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class finding_greater_number {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+     
+    System.out.println("enter the number 1"); 
+    int num1 =sc.nextInt();//taking input
+    System.out.println("enter the number 2");
+    int num2=sc.nextInt();//taking input 
+    
+
+    }
+    
+}
---
+++ multipy.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class multipy {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+     
+    System.out.println("enter the number 1"); 
+    float num1 =sc.nextFloat();//taking input
+    System.out.println("enter the number 2");
+    float num2=sc.nextFloat();//taking input 
+
+    float multipy=(num1*num2);
+    System.out.println(multipy);
+    }
+}
