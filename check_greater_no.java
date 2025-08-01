da82f4441d2fcf94c136beddeccaf429947e9a45
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2025-08-01 09:29:45 +0530

    positive negative number

--- check_greater_no.java
+++ check_greater_no.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
-public class finding_greater_number {
+public class check_greater_no {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+          Scanner sc = new Scanner(System.in);
      
     System.out.println("enter the number 1"); 
     int num1 =sc.nextInt();//taking input
     System.out.println("enter the number 2");
     int num2=sc.nextInt();//taking input 
-    
+     if (num % 2 != 0) {
+            System.out.println(num + " is odd");
+        } else {
+            System.out.println(num + " is even");
+        }
 
     }
-    
 }
