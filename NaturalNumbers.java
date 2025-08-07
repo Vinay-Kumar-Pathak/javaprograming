29b95bc07b0e26514994ed635e28cdd12ed58b93
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2025-08-07 14:14:04 +0530

    natural number from 1 to n

---
+++ NaturalNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class NaturalNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number (n): ");
+        int n = scanner.nextInt();
+        
+        int i = 1;
+
+        System.out.println(n);
+        while (i <= n) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
