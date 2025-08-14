0d052688ecb02cadcfb74433075b91305ed9cbc0
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2025-08-14 15:37:45 +0530

    basaic mcq of aaray

---
+++ ARRAY CODES/mcq.java
@@ -0,0 +1,15 @@
+public class mcq {
+    public static void main(String[] args) {
+    // int[] a = new int[3];
+    //System.out.println(a[0]);
+
+    /*int[] nums = {1, 2, 3};
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = nums[i] * 5 ;
+        }
+        System.out.println(nums[2]);*/
+
+        
+
+}
+}
