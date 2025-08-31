43067ae11e82d0edc806386f7956c99ace17aa07
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2025-08-31 11:36:39 +0530

    even or odd number from 1 to n

--- loop 50 codes/even_no.java
+++ loop 50 codes/even_no.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class even_no {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int i =1;
+        /*while(i<=number){ //even no printing 
+            if(i%2==0){
+            System.out.println( i); 
+            }
+            i++;    
+        }*/
+     //odd no printing
+    while(i<=number){
+        if(i%2 !=0){
+            System.out.println(i);
+        }
+        i++;
+    }
+    }
 }
