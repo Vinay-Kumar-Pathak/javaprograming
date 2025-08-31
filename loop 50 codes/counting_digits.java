1ba8b541ca69a8ef9c624f768f2a015f629174b1
Vinay-Kumar-Pathak <vinaypathakvp31@gmail.com>
2025-08-31 20:55:37 +0530

    som of any given digit

--- loop 50 codes/counting_digits.java
+++ loop 50 codes/counting_digits.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class counting_digits {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number=sc.nextInt();
+        int count = 0;
+        int digit = number;
+        while(digit !=0){
+            digit/=10;
+            count++;
+        }
+        System.out.println(count);
+    }
 }
---
+++ sum_digit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class sum_digit {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int number= sc.nextInt();
+        int sum= 0;
+        int temp=number;
+        while(temp>0){
+          int digit=temp%10;
+          sum+=digit;
+          temp/=10;
+        }
+        System.out.println(sum);
+    }
+}
